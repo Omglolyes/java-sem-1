75b6182e0b702994d3f4c14ea5b3546db365d4af
Raiden Erdmann <raiden.erdmann@gmail.com>
2020-12-21 15:29:09 +0100

    Aufgabenblätterhinzugefügt, UE8

---
+++ UR08/UE08A2.java
@@ -0,0 +1,16 @@
+package UR08;
+
+public class UE08A2 {
+    // 6 System.out.println(erstePerson.getVorname());
+    // Dagobert
+    // 7 System.out.println(erstePerson.getNachname());
+    // Reicher
+    // 8 System.out.println(erstePerson.getSteuerklasse());
+    // 1
+    // 9 System.out.println(zweitePerson.getVorname());
+    // Donald
+    // 10 System.out.println(zweitePerson.getNachname());
+    // Arm
+    // 11 System.out.println(zweitePerson.getSteuerklasse());
+    // 3
+}
